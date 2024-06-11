9851e123d626b98a76de1d2da15bf1427199893c
serjio <komegunovsergej@gmail.com>
2024-06-11 17:31:45 +0300

    first

---
+++ src/script.js
@@ -0,0 +1,114 @@
+document.getElementById("defaultOpen").click();
+
+function openMainTab(evt, tabName) {
+    document.getElementById("mainButtons").style.display = "none";
+    document.getElementById("backButton").style.display = "block";
+    document.getElementById(tabName).style.display = "block";
+}
+
+function goBack() {
+    var tabcontents = document.getElementsByClassName("main-tabcontent");
+    for (var i = 0; i < tabcontents.length; i++) {
+        tabcontents[i].style.display = "none";
+    }
+    document.getElementById("mainButtons").style.display = "block";
+    document.getElementById("backButton").style.display = "none";
+}
+
+function showTab(tabName) {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+function calculateDistance() {
+    const diameterInput = document.getElementById("diameter");
+    const distanceToTopInput = document.getElementById("distanceToTop");
+    const indexInput = document.getElementById("index");
+    const resultElement = document.getElementById("result");
+
+    const diameter = parseFloat(diameterInput.value);
+    const distanceToTop = parseFloat(distanceToTopInput.value);
+    const index = parseFloat(indexInput.value);
+
+    if (isNaN(diameter) || diameter <= 0 || isNaN(distanceToTop) || distanceToTop < 0 || isNaN(index) || index < 0) {
+        resultElement.textContent = "Пожалуйста, введите корректные данные.";
+        return;
+    }
+
+    const distanceToCenter = calculateDistanceToCenter(diameter, distanceToTop, index);
+    resultElement.textContent = "Расстояние от центра диаметра до вершины паза: " + distanceToCenter.toFixed(3);
+}
+
+function calculateGapWidth() {
+    const gapWidthInput = document.getElementById("gapWidth");
+    const indexInputtwo = document.getElementById("indextwo");
+    const resultElement = document.getElementById("gapResult");
+
+    const gapWidth = parseFloat(gapWidthInput.value);
+    const indextwo = parseFloat(indexInputtwo.value);
+
+    if (isNaN(gapWidth) || gapWidth < 0 || isNaN(indextwo) || indextwo < 0) {
+        resultElement.textContent = "Пожалуйста, введите корректные данные.";
+        return;
+    }
+
+    const gapWidthCalculated = calculateGapWidthValue(gapWidth, indextwo);
+    resultElement.textContent = "Номинальная ширина паза " + gapWidth + " и допуск " + indextwo + " середина паза равна " + gapWidthCalculated.toFixed(4);
+}
+
+function calculateDistanceToCenter(diameter, distanceToTop, index) {
+    const slot = distanceToTop - diameter;
+    const radius = diameter / 2;
+    const coefficient = index / 2;
+
+    return slot + radius + coefficient;
+}
+
+function calculateGapWidthValue(indextwo, gapWidth) {
+    const mediumWidth = gapWidth / 4;
+    const coefficient1 = indextwo / 2;
+
+    return coefficient1 + mediumWidth;
+}
+
+function calculateCoordinates(point) {
+    let diameterInput, angleInput, cutterDiameterInput, resultElement;
+
+    if (point === 'bottom') {
+        diameterInput = document.getElementById("diameterBottom");
+        angleInput = document.getElementById("angleBottom");
+        cutterDiameterInput = document.getElementById("cutterDiameterBottom");
+        resultElement = document.getElementById("resultBottom");
+    } else {
+        diameterInput = document.getElementById("diameterTop");
+        angleInput = document.getElementById("angleTop");
+        cutterDiameterInput = document.getElementById("cutterDiameterTop");
+        resultElement = document.getElementById("resultTop");
+    }
+
+    const diameter = parseFloat(diameterInput.value);
+    const angle = parseFloat(angleInput.value);
+    const cutterDiameter = parseFloat(cutterDiameterInput.value);
+
+    if (isNaN(diameter) || diameter <= 0 || isNaN(angle) || angle < 0 || angle > 360 || isNaN(cutterDiameter) || cutterDiameter < 0) {
+        resultElement.textContent = "Пожалуйста, введите корректные данные.";
+        return;
+    }
+
+    const radius = diameter / 2;
+    const angleInRadians = (angle * Math.PI) / 180;
+    let x, y;
+
+    if (point === 'bottom') {
+        x = (radius - cutterDiameter) * Math.cos(angleInRadians);
+        y = (radius - cutterDiameter) * Math.sin(angleInRadians);
+    } else {
+        x = (radius + cutterDiameter) * Math.cos(angleInRadians);
+        y = (radius + cutterDiameter) * Math.sin(angleInRadians);
+    }
+
+    resultElement.textContent = `Координаты точки: X = ${x.toFixed(2)}, Y = ${y.toFixed(2)}`;
+}
